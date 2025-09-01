c177b119e2c4059d1049a25f9a27022aa24750a9
Andrii Kyrychenko <akyrych84@gmail.com>
2025-09-01 10:00:43 +0200

    Initial Commit

---
+++ TRoulette/TRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRouletteApp.swift
+//  TRoulette
+//
+//  Created by Andrii Kyrychenko on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
