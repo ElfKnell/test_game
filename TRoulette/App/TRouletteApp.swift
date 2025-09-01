0d28e94ce007e3c99225b760aa831e3a3e17b940
Andrii Kyrychenko <akyrych84@gmail.com>
2025-09-01 10:45:12 +0200

    added firebase

---
+++ TRoulette/App/TRouletteApp.swift
@@ -0,0 +1,30 @@
+//
+//  TRouletteApp.swift
+//  TRoulette
+//
+//  Created by Andrii Kyrychenko on 01/09/2025.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct TRouletteApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
